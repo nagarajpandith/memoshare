67a3b71cc2a0e2ee37dd9528c9589f897eaefcaf
Tushar Karkera <tusharvkarkera1@gmail.com>
2024-03-19 20:28:39 +0530

    fix: Login Page import

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import MemoDashboard from "./components/MemoDashboard";
 function App() {
   return (
     <main className="min-h-screen flex justify-center items-center flex-col" style={{background: "#FBF3D5"}}>
+      {/* <LoginPage> */}
       <MemoDashboard/>
     </main>
   );
