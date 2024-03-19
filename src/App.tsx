d006082a75baa28ea9d163d51c5d64137c9f91b0
Tushar Karkera <tusharvkarkera1@gmail.com>
2024-03-19 12:56:08 +0530

    Implimented Login page - Implemented email enter and save - Styled the Login page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import LoginPage from "./components/LoginPage";
+
 function App() {
   return (
-    <main className="min-h-screen flex justify-center items-center flex-col">
-      <h1 className="text-3xl font-bold underline mb-5">Memo Share</h1>
-      <p>A Note sharing app to practice Azure logic app and React.</p>
+    <main className="min-h-screen flex justify-center items-center flex-col" style={{background: "#FBF3D5"}}>
+      <LoginPage/>
     </main>
   );
 }
---
+++ src/components/LoginPage.tsx
@@ -0,0 +1,53 @@
+import { useState } from "react";
+import { TextField, Button, Box } from "@mui/material";
+
+const LoginPage = () => {
+    const [email, setEmail] = useState("")
+    const [emailError, setEmailError] = useState(false)
+
+    const handleSubmit = () => {
+        if (email == '') {
+            setEmailError(true)
+        } else {
+            const emailCred = JSON.stringify(localStorage.getItem('EmailCred'))
+            if (emailCred != "null") {
+                /// continue with the found email => emailCred
+            } else {
+                localStorage.setItem("EmailCred", email)
+            }
+        }
+    }
+
+    return (
+        <Box className="flex justify-center items-center p-8 shadow-md rounded-3xl flex-wrap" style={{ background: "#F5E8DD" }}>
+            <div
+                className="w-full text-center"
+                style={{ fontFamily: "Delicious Handrawn", fontSize: "2em" }}
+            >👋 Welcome to </div>
+            <div>
+                <h2
+                    className="text-center"
+                    style={{ fontFamily: "Delicious Handrawn", fontSize: "4em" }}
+                >MemoShare
+                </h2>
+                <TextField
+                    onChange={(e:any) => setEmail(e.target.value)}
+                    required
+                    placeholder="Enter your email to get started..."
+                    variant="standard"
+                    color="secondary"
+                    type="email"
+                    sx={{ bottom: 3 }}
+                    value={email}
+                    error={emailError}
+
+                />
+                <Button variant="text" color="primary" onClick={handleSubmit} >
+                    <img src="src\assets\enter.png" className="size-7 p-0" />
+                </Button>
+            </div>
+        </Box>
+    );
+}
+
+export default LoginPage;
