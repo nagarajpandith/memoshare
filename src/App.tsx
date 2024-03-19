86cebdf3c51e192a4543d27b091ee819b05297d8
Tushar Karkera <tusharvkarkera1@gmail.com>
2024-03-19 20:27:27 +0530

    Implemented MemoShare Dashboard - Create Button - List of memo with dummy data

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import LoginPage from "./components/LoginPage";
+// import LoginPage from "./components/LoginPage";
+import MemoDashboard from "./components/MemoDashboard";
 
 function App() {
   return (
     <main className="min-h-screen flex justify-center items-center flex-col" style={{background: "#FBF3D5"}}>
-      <LoginPage/>
+      <MemoDashboard/>
     </main>
   );
 }
---
+++ src/data/DummyData.tsx
@@ -0,0 +1,16 @@
+export interface Memo {
+    Title:String,
+    Shared:String
+}
+
+export const data:Memo[] = [
+    {Title:"Title 1", Shared:"4"},
+    {Title:"Title 2", Shared:"1"},
+    {Title:"Title 3", Shared:"3"},
+    {Title:"Title 4", Shared:"6"},
+    {Title:"Title 5", Shared:"2"},
+    {Title:"Title 6", Shared:"8"},
+    {Title:"Title 7", Shared:"1"},
+    {Title:"Title 8", Shared:"7"},
+
+]
